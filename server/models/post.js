de01f75c7d1d4327dfd113b664bd659ad4e9cbda
Philip Vo <philiptranbavo@gmail.com>
2016-11-03 18:23:33 -0700

    First commit.

--- server/models/post.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-var PostSchema = new mongoose.Schema({
-	_user: {type: Schema.Types.ObjectId, ref: 'User'},
-	image: {type: String, required: true},
-	links: [{type: String}]
-	// links: [{type: Schema.Types.ObjectId, ref: 'Link'}]
-}, {timestamps: true});
-mongoose.model('Post', PostSchema);
