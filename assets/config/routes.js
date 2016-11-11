7b383de9311a349533d53924983ff7f7cfe0bebd
Philip Vo <philiptranbavo@gmail.com>
2016-11-11 10:15:01 -0800

    Stardate.

--- assets/config/routes.js
+++ assets/config/routes.js
@@ -2,24 +2,31 @@ var app = angular.module('app', ['ngRoute']);
 app.config(function ($routeProvider) {
 	$routeProvider
 	.when('/',{
-		templateUrl: 'partials/index.html',
-		controller: 'indexController'
+		templateUrl: 'partials/index.html'
 	})
 	.when('/about',{
-		templateUrl: 'partials/about.html',
-		controller: 'indexController'
+		templateUrl: 'partials/about.html'
 	})
 	.when('/skills',{
-		templateUrl: 'partials/skills.html',
-		controller: 'indexController'
+		templateUrl: 'partials/skills.html'
 	})
 	.when('/projects',{
-		templateUrl: 'partials/projects.html',
-		controller: 'indexController'
-	})		
+		templateUrl: 'partials/projects.html'
+	})
+	.when('/froxxi',{
+		templateUrl: 'partials/froxxi.html'
+	})
+	.when('/stardate',{
+		templateUrl: 'partials/stardate.html'
+	})
+	.when('/snippet',{
+		templateUrl: 'partials/snippet.html'
+	})
+	.when('/localator',{
+		templateUrl: 'partials/loca.html'
+	})
 	.when('/contact',{
-		templateUrl: 'partials/contact.html',
-		controller: 'indexController'
+		templateUrl: 'partials/contact.html'
 	})	
 	.otherwise({
 		redirectTo: '/'
